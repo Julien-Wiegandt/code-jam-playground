691b9b0da5ded994aeb8d6be9ef3172d08da6817
Julien-Wiegandt <julienwiegandt@gmail.com>
2022-01-22 14:46:03 -0500

    algo fix

--- others/binary_trees.py
+++ others/binary_trees.py
@@ -1,7 +1,6 @@
 # From a BDD and one of his state, return the boolean formula from the state
 def bddToBooleans(bdd, sommet):
     res = []
-
     # --- Find all sucessful end state (lo or hi == 1)--- #
     for row in bdd:
         if(row[2] == 1):
@@ -71,23 +70,47 @@ def bddToBooleans(bdd, sommet):
         finalStr += ")"
     return finalStr
 
-# Create/Find a state in the BDD where the boolean formula in the opposite from the state parameter
+# Inverse les 0 et 1 sur tout le BDD, créé les sommets voulus si necessaire
 def revertFormula(bdd, sommet):
-    if(sommet == 0 or sommet == 1):
-        return sommet
-    for row in bdd:
-        if(row[0] == sommet):
-            exist = False
-            lo = revertFormula(bdd, row[3])
-            hi = revertFormula(bdd, row[2])
-            for state in bdd:
-                if(row[1] == state[1] and lo == state[2] and hi == state[3]):
-                    exist = True
-                    return state[0]
-            if (not exist):
-                newState = [len(bdd),row[1],lo,hi]
-                bdd.append(newState)
-                return newState[0]
+    if(sommet == 0):
+        return 1
+    if(sommet == 1):
+        return 0
+
+    exist = False
+    lo = revertFormula(bdd, bdd[sommet][2])
+    hi = revertFormula(bdd, bdd[sommet][3])
+    for state in bdd:
+        if(bdd[sommet][1] == state[1] and lo == state[2] and hi == state[3]):
+            exist = True
+            return state[0]
+        if (not exist):
+            newState = [len(bdd),bdd[sommet][1],lo,hi]
+            bdd.append(newState)
+            return newState[0]
+
+def minWay(bdd, sommet):
+    if(sommet == 1):
+        return []
+    elif(bdd[sommet][2] == 0):
+        dic = minWay(bdd, bdd[sommet][3])
+        dic.append(1)
+        return dic
+    else:
+        dic = minWay(bdd, bdd[sommet][2])
+        dic.append(0)
+        return dic
+
+# Prend en entré le tableau de bit comme il ressort de minWay (à l'envers)
+# En sortie : Le nombre entier correspondant aux bits d'entré
+def binaryToInt(tab):
+    val = 0
+    for i in range(len(tab)-1, -1, -1):
+        if(i == 0 and tab[i] == 1):
+            val += 1
+        else:
+            val += tab[i] * i * 2
+    return val 
 
 BDD1 = [
     [0,"X","-","-"],
@@ -103,12 +126,30 @@ BDD2 = [
     [1,"X","-","-"],
     [2,"X2",1,0],
     [3,"X2",0,1],
-    [4,"X3",2,3]
+    [4,"X1",2,3]
 ]
 
-BDD = BDD2
-sommet = 4
-print(bddToBooleans(BDD, sommet))
-res = revertFormula(BDD, sommet)
-print(res)
-print(bddToBooleans(BDD, res))
+BDD3 = [
+    [0,"X","-","-"],
+    [1,"X","-","-"],
+    [2,"X4",0,1],
+    [3,"X4",0,1],
+    [4,"X3",1,2],
+    [5,"X3",0,3],
+    [6,"X2",5,4],
+    [7,"X2",0,5],
+    [8,"X1",7,6]
+]
+
+BDD = BDD3
+sommet = 8
+
+# print(bddToBooleans(BDD, sommet))
+# res = revertFormula(BDD, sommet)
+# print(res)
+# print(bddToBooleans(BDD, res))
+# print(BDD)
+
+tab = minWay(BDD, sommet)
+print(tab)
+print(binaryToInt(tab))
